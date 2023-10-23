73437279212d4a9b40369e2006585730fecaec4b
Ruslan Musienko <106426703+gamarjkeee@users.noreply.github.com>
2023-10-23 12:39:12 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -37,6 +37,13 @@
     function addTouchClass() {
         if (isMobile.any()) document.documentElement.classList.add("touch");
     }
+    function addLoadedClass() {
+        if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
+            setTimeout((function() {
+                document.documentElement.classList.add("loaded");
+            }), 0);
+        }));
+    }
     function getHash() {
         if (location.hash) return location.hash.replace("#", "");
     }
@@ -778,6 +785,7 @@
     window["FLS"] = false;
     isWebp();
     addTouchClass();
+    addLoadedClass();
     menuInit();
     fullVHfix();
     pageNavigation();
